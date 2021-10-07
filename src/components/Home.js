c04ab0b4db21dc3b1532041e6f8989295d8c2a15
Lindsey <linds.fonnes@gmail.com>
2021-10-06 21:07:41 -0600

    home page and navbar styled

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import UserContext from "../UserContext";
+import "./styles/Home.css";
 
 function Home() {
-  return <h1>Home Page</h1>;
+  const { currentUser } = useContext(UserContext);
+
+  return (
+    <div className="Home">
+      <div className="container text-center">
+        <h1 className="font-weight-bold">Jobly</h1>
+        <p className="lead mt-4">All the jobs in one, convenient place.</p>
+        {currentUser ? (
+          <h2>Welcome Back, {currentUser.firstName} </h2>
+        ) : (
+          <div>
+            <Link
+              to="/login"
+              className="Home-button btn btn-info font-weight-bold"
+            >
+              Log In
+            </Link>
+            <Link
+              to="/signup"
+              className="Home-button btn btn-info font-weight-bold"
+            >
+              Sign Up
+            </Link>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Home;
--- src/routing/Nav.js
+++ src/routing/Nav.js
@@ -1,44 +1,61 @@
 import React, { useContext } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 import UserContext from "../UserContext";
+import "../components/styles/Nav.css";
 
 function NavBar({ logout }) {
   const { currentUser } = useContext(UserContext);
 
   function loggedInUserNav() {
     return (
-      <nav className="NavBar">
-        <NavLink exact to="/">
+      <nav className="NavBar navbar navbar-expand-lg">
+        <NavLink className="navbar-brand" exact to="/">
           Jobly
         </NavLink>
-        <NavLink exact to="/companies">
-          Companies
-        </NavLink>
-        <NavLink exact to="/jobs">
-          Jobs
-        </NavLink>
-        <NavLink exact to="/profile">
-          Profile
-        </NavLink>
-        <NavLink exact to="/" onClick={logout}>
-          Log Out
-        </NavLink>
+        <ul className="NavBar-links navbar-nav">
+          <li className="nav-item">
+            <NavLink className="nav-link" exact to="/companies">
+              Companies
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" exact to="/jobs">
+              Jobs
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" exact to="/profile">
+              Profile
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" exact to="/" onClick={logout}>
+              Log Out
+            </Link>
+          </li>
+        </ul>
       </nav>
     );
   }
 
   function loggedOutUserNav() {
     return (
-      <nav>
-        <NavLink exact to="/">
+      <nav className="NavBar navbar navbar-expand-lg">
+        <NavLink className="navbar-brand NavBar-brand" exact to="/">
           Jobly
         </NavLink>
-        <NavLink exact to="/login">
-          Login
-        </NavLink>
-        <NavLink exact to="/signup">
-          Sign Up
-        </NavLink>
+        <ul className="NavBar-links navbar-nav">
+          <li className="nav-item">
+            <NavLink className="nav-link" exact to="/login">
+              Login
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" exact to="/signup">
+              Sign Up
+            </NavLink>
+          </li>
+        </ul>
       </nav>
     );
   }
